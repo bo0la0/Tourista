2cfc3e63099d918bb7e41eb453d1b5d649d071ed
Boola <boolaraouf@outlook.com>
2022-04-08 03:52:03 +0200

    button

---
+++ lib/shared/components/components.dart
@@ -0,0 +1,21 @@
+  import 'package:flutter/material.dart';
+
+Widget defaultButton({
+    @required double width =  double.infinity,
+    @required Color background = Colors.blue,
+    @required Function function,
+    @required String text,
+  }) => Container(
+  width: width,
+  color: background,
+  child:  MaterialButton(
+
+    onPressed: function,
+    child: Text(
+      text.toUpperCase(),
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+  ),
+);
